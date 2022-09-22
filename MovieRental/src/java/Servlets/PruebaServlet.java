74c4295ad368995803eef4745c4b43bdba54ab52
dianamopan <dianamopan1@gmail.com>
2022-09-22 17:49:18 -0500

    Initial commit

---
+++ MovieRental/src/java/Servlets/PruebaServlet.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Servlets;
+
+import java.io.IOException; //Excepciones
+import java.io.PrintWriter; //Escribir. Enviar a html a través de Java
+import javax.servlet.ServletException; //Excepciones de conexion y BD
+import javax.servlet.annotation.WebServlet; //Notacion de webServlet (permite tener enlace)
+import javax.servlet.http.HttpServlet; //Permite comunicarse con la web
+import javax.servlet.http.HttpServletRequest; //Puedo hacer peticiones
+import javax.servlet.http.HttpServletResponse; //Y recibir una respuesta
+
+/**
+ *
+ * @author Usuario
+ */
+@WebServlet(name = "PruebaServlet", urlPatterns = {"/PruebaServlet"})
+public class PruebaServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            String id = request.getParameter("id");
+            String name = request.getParameter("name");
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet PruebaServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet PruebaServlet at " + request.getContextPath() + "</h1>");
+            response.getWriter().append("El id fue recibido y es: " + id + " y el nombre de la pelicula es: " + name);
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
